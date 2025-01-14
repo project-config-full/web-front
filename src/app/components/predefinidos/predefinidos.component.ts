6fcd5672f224b8caf0b857ab80ee332b01c404eb
Luiz Felipe <luizfelipealvesdematos10@gmail.com>
2025-01-14 20:13:40 -0300

    organizando arquivos

---
+++ src/app/components/predefinidos/predefinidos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-predefinidos',
+  standalone: true,
+  imports: [],
+  templateUrl: './predefinidos.component.html',
+  styleUrl: './predefinidos.component.css'
+})
+export class PredefinidosComponent {
+
+}
