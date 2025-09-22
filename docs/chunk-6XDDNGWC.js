b7d3ae03708eb70af5efae36880273ac2c5bc033
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-09-22 18:14:25 -0300

    Iniciando na "reforma" do projeto

--- docs/chunk-6XDDNGWC.js
+++
@@ -1,1 +0,0 @@
-import{Oa as a}from"./chunk-L3HG6L4V.js";export{a as AppComponent};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-      provideZoneChangeDetection({ eventCoalescing: true }),
-      provideRouter(routes),
-      provideAnimations()
-    ]
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
